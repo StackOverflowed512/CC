53d179eff1cd452f7affac86d4f30758513e6889
StackOverflowed512 <omkarmutyalwar5@gmail.com>
2026-05-14 18:28:51 +0530

    remove

--- blog_mern_app/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Kill any process on port 5003
-sudo fuser -k 5003/tcp 2>/dev/null
-cd backend && npm start
--- student_mern_app/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Kill any process on port 5001
-sudo fuser -k 5001/tcp 2>/dev/null
-cd backend && npm start
